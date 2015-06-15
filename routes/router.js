eb33737f2aea97c3f9e127d8a20544d467e56718
Rafael Oliveira <rafamelo.oliveira@gmail.com>
2015-06-14 22:44:38 -0300

    Added the functionality to get the top artists for each related artist given an artist.

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,10 @@
 module.exports = function(app, spotifyWebApi){
 
 	var spotify = new spotifyWebApi({
-		clientId: '<your_id_here>',
-		clientSecret: '<your_secret_here>'
+		clientId: '9ab7d2b02b3a4e2bb07b02ff0bf6da61',
+		clientSecret: 'b8e0044da9d24edd9d1d3d85c814112f'
 	});
-	// spotify.setAccessToken('BQD0ZfwCEpH4-U1jOaUtbBVt0yxQvJV-dVGDoPBdbBL5GH0edRhnCcZPm3JP3rQAZ4zdH6Qh3sSDBj80b0_EjbWKPUp9aqPeS6h1zBeCQtSeBng3heRnb2H8zn-wIRoTR6GSjYgBdeIrLMxSlCxvj6nbX9nWyvdXtnpeyEWv68RKGjfeR0avGSX17uXN');
+	
 	app.get('/', function(req, res){
 		res.render('index.ejs');
 	});
@@ -18,14 +18,28 @@ module.exports = function(app, spotifyWebApi){
 			spotify.getArtistRelatedArtists(artistId)
 			.then(function(data2){
 				if(data2.body.artists.length){
+					console.log(data2.body);
 					console.log('I\'ve got ' + data2.body.artists.length + ' similar artists');
-					// console.log(data2.body.artists);
+					console.log(data2.body.artists[0].id);
 					for(var i = 0; i < data2.body.artists.length; i++){
 						relatedArtists.push(data2.body.artists[i].name);
 						// console.log(relatedArtists);
 						
 					}
-					console.log(relatedArtists);
+
+					data2.body.artists.forEach(function(obj){
+						spotify.getArtistTopTracks(obj.id, 'BR')
+						.then(function(data3){
+							console.log('============================');
+							console.log(obj.name);
+							data3.body.tracks.forEach(function(track, index){
+
+								console.log((index+1) + '. ' + track.name);
+							});
+						});
+						
+					});
+					// console.log(relatedArtists);
 					res.render('related_artists.ejs', {
 						relatedArtists: relatedArtists
 					});
@@ -42,5 +56,20 @@ module.exports = function(app, spotifyWebApi){
 			console.log(err);
 			// res.render('error.ejs');
 		});
+	});
+
+	app.get('/topTracksArtist', function(req, res){
+		spotify.getArtistTopTracks('5IHkB1o1qNXwLPe55WWTEH', 'BR')
+		.then(function(data){
+			data.body.tracks.forEach(function(track, index){
+				console.log((index+1) + '. ' + track.name + ' (popularity is ' + track.popularity + ')');
+			});
+			res.end();
+		});
+	});
+
+
+	app.get('/generatePlaylist', function(req, res){
+		spotify.authorizationCodeGrant(authorizationcode)
 	});
 }
